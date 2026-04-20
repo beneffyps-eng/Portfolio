4619f48a3cc90909abf39d3eb1ddbbd303a6721b
Beneffyps <beneffyps@gmail.com>
2026-04-20 14:18:47 -0800

    initial portfolio upload

---
+++ tailwindconfiguration.js
@@ -0,0 +1,13 @@
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#00ff88',   // green
+          secondary: '#3b82f6', // blue  
+          darkbg: '#0b1220'     // dark navy
+        }
+      }
+    }
+  }
+</script>
